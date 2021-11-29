4c755ac9d3a986a3b289208bfafe75ec8ee520ae
Kseniya <demchuk944@gmail.com>
2021-11-29 17:11:33 +0300

    lesson-4

--- index.js
+++ index.js
@@ -1,51 +1,66 @@
 const fs = require("fs");
+const yargs = require("yargs");
+const inquirer = require("inquirer");
+const { join } = require("path");
 
-const ip1 = /89.123.1.41/,
-  ip2 = /34.48.240.111/,
-  FileName1 = "./89.123.1.41_requests.log",
-  FileName2 = "./34.48.240.111_requests.log",
-  readStream = fs.createReadStream("./access.log", {
-    encoding: "utf8",
-    highWaterMark: 64 * 1024,
-  }),
-  writeStream1 = fs.createWriteStream(FileName1, {
-    flags: "a",
-    encoding: "utf8",
-  }),
-  writeStream2 = fs.createWriteStream(FileName2, {
-    flags: "a",
-    encoding: "utf8",
-  });
+const options = yargs
+  .usage("Usage: -d <dir>")
+  .option("d", {
+    alias: "dir",
+    describe: "Directory",
+    type: "string",
+    demandOption: true,
+  }).argv;
 
-let readed = 0;
-var block = 0;
-fs.stat("./access.log", (err, stats) => (block = stats.size / 50));
-let counter = 1;
-readStream.on("data", (chunk) => {
-  let arr = chunk.split("\n");
-  arr.forEach((item) => {
-    if (ip1.test(item)) {
-      writeStream1.write(item + "\n");
-    }
-    if (ip2.test(item)) {
-      writeStream2.write(item + "\n");
-    }
-  });
-  readed = readed + 64 * 1024;
-  if (readed > block * counter) {
-    counter++;
-    process.stdout.write(`#`);
-  }
-});
+let curDir = options.dir;
+let list = [];
 
-readStream.on("end", () => {
-  console.log("\n Close reading stream.");
-  writeStream1.end(() => console.log(`File ${FileName1} writing finished.`));
-  writeStream2.end(() => console.log(`File ${FileName2} writing finished.`));
-  fs.stat(FileName1, (err, stats) =>
-    console.log(`File ${FileName1} size ${stats.size} bytes`)
-  );
-  fs.stat(FileName2, (err, stats) =>
-    console.log(`File ${FileName2} size ${stats.size} bytes`)
-  );
-});
+try {
+  list = fs.readdirSync(curDir);
+} catch (err) {
+  curDir = process.cwd();
+  list = fs.readdirSync(curDir);
+  console.log(`No such directory... Directory set to ${curDir}`);
+}
+
+const getFile = (list) => {
+  let isDir = true;
+
+  inquirer
+    .prompt([
+      {
+        name: "element",
+        type: "list",
+        message: "Choose file:",
+        choices: list,
+      },
+    ])
+    .then((answer) => answer.element)
+    .then((element) => {
+      let filePath = join(curDir, element);
+      if (fs.lstatSync(filePath).isFile()) {
+        data = fs.readFileSync(filePath, "utf8");
+        inquirer
+          .prompt([
+            {
+              name: "search",
+              type: "string",
+              message: "Enter search string",
+            },
+          ])
+          .then((answer) => answer.search)
+          .then((searchString) => {
+            console.log(data.match(new RegExp(`${searchString}`, "g")));
+          });
+        isDir = false;
+      } else {
+        curDir = join(curDir, element);
+        list = fs.readdirSync(curDir);
+        getFile(list);
+      }
+    });
+
+  return isDir;
+};
+
+isDir = getFile(list);
