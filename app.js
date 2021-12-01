20f6b0d4da1de9f9e108d4188dedeba2b86cb4a7
Kseniya <demchuk944@gmail.com>
2021-12-02 00:13:32 +0300

    lesson-6

--- app.js
+++ app.js
@@ -1,57 +1,77 @@
 const http = require("http");
+const io = require("socket.io");
 const path = require("path");
 const fs = require("fs");
+let users = [
+  {
+    id: "",
+    name: "David",
+  },
+  {
+    id: "",
+    name: "Leonardo",
+  },
+  {
+    id: "",
+    name: "Stiv",
+  },
+  {
+    id: "",
+    name: "Sara",
+  },
+];
+let counter = 0;
 
-http
-  .createServer((request, response) => {
-    if (request.method === "GET") {
-      let filePath = "";
-      switch (request.url) {
-        case "/style.css":
-          filePath = path.join(__dirname, "style.css");
-          readStream = fs.createReadStream(filePath);
-          response.writeHead(200, { "Content-Type": "text/css" });
-          readStream.pipe(response);
-          break;
-        default:
-          filePath = path.join(__dirname, "index.html");
-          readStream = fs.createReadStream(filePath);
-          response.writeHead(200, { "Content-Type": "text/html" });
-          readStream.pipe(response);
-      }
-    } else {
-      if (request.method === "POST") {
-        let receivedData = "";
-        request.on("data", (chunk) => {
-          receivedData += chunk;
-        });
-        request.on("end", () => {
-          let data = JSON.parse(receivedData);
-          let filePath = path.join(
-            data.parentDir === "/" ? __dirname : data.parentDir,
-            data.element
-          );
-          if (data.element === "home") {
-            filePath = path.dirname(data.parentDir);
-          }
+const app = http.createServer((request, response) => {
+  if (request.method === "GET") {
+    const filePath = path.join(__dirname, "index.html");
 
-          if (fs.lstatSync(filePath).isFile()) {
-            readStream = fs.createReadStream(filePath);
-            response.writeHead(200, { "Content-Type": "text/html" });
-            readStream.pipe(response);
-          } else {
-            data = {
-              parentDir: filePath,
-              list: fs.readdirSync(filePath),
-            };
-            response.writeHead(200, { "Content-Type": "json" });
-            console.log(JSON.stringify(data));
-            response.end(JSON.stringify(data));
-          }
-        });
-      } else {
-        response.end("Method Not Allowed");
-      }
-    }
-  })
-  .listen(3000, "localhost");
+    readStream = fs.createReadStream(filePath);
+
+    readStream.pipe(response);
+  } else if (request.method === "POST") {
+    let data = "";
+
+    request.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    request.on("end", () => {
+      const parsedData = JSON.parse(data);
+      console.log(parsedData);
+
+      response.writeHead(200, { "Content-Type": "json" });
+      response.end(data);
+    });
+  } else {
+    response.statusCode = 405;
+    response.end();
+  }
+});
+
+const socket = io(app);
+
+socket.on("connection", function (socket) {
+  let newOne = users.find((user) => user.id === "");
+  counter++;
+  newOne.id = socket.id;
+  console.log(`Client ${newOne.name} just connected to Server`);
+  socket.broadcast.emit("NEW_CONN_EVENT", {
+    msg: `${newOne.name} joined chat... Now ${counter} active clients`,
+  });
+  socket.on("CLIENT_MSG", (data) => {
+    let text = data.msg.split("").reverse().join("");
+    socket.emit("SERVER_MSG", { msg: text });
+    socket.broadcast.emit("SERVER_MSG", { msg: text });
+  });
+  socket.on("disconnect", (reason) => {
+    counter--;
+    console.log(
+      `${
+        users.find((user) => user.id === socket.id).name
+      } left chat (reason: ${reason}) / Now ${counter} active clients`
+    );
+  });
+});
+
+app.listen(3000, "localhost");
